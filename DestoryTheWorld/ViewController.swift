c33c92083f0caa639516d2645b922bec98ffbea9
Lancia Draper <Lancia.draper@mail.com>
2020-04-16 19:17:52 -0700

    Initial Commit

---
+++ DestoryTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DestoryTheWorld
+//
+//  Created by lancia draper on 4/16/20.
+//  Copyright © 2020 lancia draper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
